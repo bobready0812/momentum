98e160cbeca922721f9a76baa5039c9528155e15
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-13 22:06:24 +0900

    20200813

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+function  sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+sayHello("nico", 10);
+sayHello("dal", 24);
+sayHello("lynn", 17);
 
-const player = {
-    name: "daehyeon",
-    points: 10,
-    fat: true,
+
+function plus(a,b) {
+console.log(a+b);
+}
+plus(11123123123, 123123123);
+
+function divide(c,d){
+    console.log(c/d);
+}
+divide(6, 3);
+
+
+const player ={
+name: "nico",
+sayHello: function(otherPersonsName) {
+    console.log("hello! " + otherPersonsName + " Nice to meet you!");
+},
 };
-console.log(player);
-player.fat =false;
-player.lastName ="potato";
-console.log(player);
+console.log(player.name)
+player.sayHello("lynn");
