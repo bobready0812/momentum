adcc14091633f554355077475a34d2694f61c331
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-13 21:33:51 +0900

    master

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const mon ="mon";
-const tue ="tue";
-const wed ="wed";
-const thu ="thu";
-const fri ="fri";
-const sat ="sat";
-const sun = "sun";
-const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
-console.log(daysOfWeek[2])
-daysOfWeek.push("weekend")
-console.log(daysOfWeek[7])
+
+const player = {
+    name: "daehyeon",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.fat =false;
+player.lastName ="potato";
+console.log(player);
