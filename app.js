2175d33f4af8d228ae5f1ed2477e500766236137
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-14 22:55:27 +0900

    20210814

--- app.js
+++ app.js
@@ -3,34 +3,14 @@ console.dir(title);
 
 
 function handleTitleClick() {
-    title.style.color = "aqua";
+  const currentColor =title.style.color;
+  let newColor;
+   if(currentColor === "blue") {
+       newColor = "tomato";
+   } else {
+       newColor = 'blue';
+   }
+   title.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    title.innerText ="Mouse is here!!";
-}
-
-function handleMouseLeave() {
-    title.innerText ="Mouse is Gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copier!");
-}
-function handleWindowOffline(){
-    alert("sos! no WIFI");
-}
-function handleWindowOnline(){
-    alert("all good!");
-}
-title.onclick = handleTitleClick;
-title.onmouseenter = handleMouseEnter;
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+title.addEventListener("click", handleTitleClick);
