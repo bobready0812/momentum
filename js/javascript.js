28c794bad0b57a2fdb272784576e485ecc5ff0ae
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-17 14:56:04 +0900

    20210817

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,51 @@
-const clock = document.querySelector("h2#clock");
-function  getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0"); 
+const quotes = [ {
+    quote: "Flattery is all right if you don't inhale.",
+    Author: "Adlai E. Stevenson Jr.",
+},
+{
+    quote: "It's the friends you can call up at four a.m. that matter.",
+    Author: "Marlene Dietrich",
+},
+{
+ quote: "We secure our friends not by accepting favors but by doing them.",
+ Author: "Thucydides",
+},
+{
+quote: "What is a friend? A single soul dwelling in two bodies.",
+Author: "Aristotle",
+},
 
-    clock.innerText =`${hours}:${minutes}:${seconds}`;
-}
+{
+quote: "He who can, does. He who cannot, teaches.",
+Author: "George Bernard Shaw",
+},
+{
+    quote: "Prosperity makes friends, adversity tries them.",
+    Author: "Publilius Syrus",
+},
+{
+    quote: "Life is a risk.",
+    Author: "Diane Frolov",
+},
+{
+    quote: "If I have lost confidence in myself, I have the universe against me.",
+    Author: "Ralph Waldo Emerson",
+},
+{
+    quote: "Love is an irresistible desire to be irresistibly desired",
+    Author: "Robert Frost",
+},
+{
+    quote: "To love is to receive a glimpse of heaven.",
+    Author: "Karen Sunde",
+} ];
+    
+            
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
 
-setInterval(getClock, 1000); 
- 
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]; 
+
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.Author;
