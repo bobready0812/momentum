be74f2ead77cb5b10a22dd8e5ad167feb1a9acea
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-18 13:33:10 +0900

    20210818

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,24 @@
+const API_KEY ="15f88eaf631746bd32fc713337bd5c9d";
+
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    
-    
-    console.log("you live in", lat, lng);
+    const log = position.coords.longitude;
+     
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+    const weather =document.querySelector("#weather span:first-child");
+    const city =document.querySelector("#weather span:last-child");
+       weather.innerText =data.weather[0].main;
+       city.innerText =data.name;
+    }); 
+    } 
 
-
-}
 function onGeoError(){
 alert("Can't find you. No weather for you");
 }
 
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition( onGeoOk, onGeoError );
