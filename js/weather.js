601ec51bf50c492ede783c949b6d51f3c9c01ab8
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-19 23:37:16 +0900

    20210819

--- js/weather.js
+++ js/weather.js
@@ -9,8 +9,8 @@ function onGeoOk(position){
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
-    const weather =document.querySelector("#weather span:first-child");
-    const city =document.querySelector("#weather span:last-child");
+    const weather =document.querySelector("#weather span:last-child");
+    const city =document.querySelector("#weather span:first-child");
        weather.innerText =data.weather[0].main;
        city.innerText =data.name;
     }); 
